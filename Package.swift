05dc18dd72de694128d5c561603ebcb9911c3e44
Francois Green <fwgreen@users.noreply.github.com>
2016-12-10 21:38:47 -0500

    Update to Vapor 1.1
    ... and switch from SQLite to PostgreSQL

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name: "PersonalGreetings",
   dependencies: [
-    .Package(url:"https://github.com/vapor/vapor.git", majorVersion: 1),
-    .Package(url:"https://github.com/vapor/sqlite-provider.git", majorVersion: 1)
+    .Package(url:"https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+    .Package(url:"https://github.com/vapor/postgresql-provider.git", majorVersion: 1, minor: 1)
   ],
   exclude: [
     "Resources"
