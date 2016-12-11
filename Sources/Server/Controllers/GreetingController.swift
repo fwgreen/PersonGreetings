647923f2bb5242710849a4b40f16b6e950f9b70f
Francois Green <fwgreen@users.noreply.github.com>
2016-12-10 21:50:39 -0500

    Add new methods

--- Sources/Server/Controllers/GreetingController.swift
+++ Sources/Server/Controllers/GreetingController.swift
@@ -3,18 +3,38 @@ import HTTP
 
 final class GreetingController {
 
+  let directives = Node(["home": "/greeting", "create": "/greeting/new", "list": "/greeting/all", "help": "/greeting/help"])
+
+  func index(_ request: Request) throws -> ResponseRepresentable {
+    return try app.view.make("greeting/index", [
+      "title": "Test App",
+      "heading": "Greetings",
+      "directives": directives
+    ])
+  }
+
+  func help(_ request: Request) throws -> ResponseRepresentable {
+    return try app.view.make("greeting/help", [
+      "title": "Test App",
+      "heading": "Greetings Help",
+      "directives": directives
+    ])
+  }
+
   func detail(_ request: Request, greeting: Greeting) throws -> ResponseRepresentable {
     return try app.view.make("greeting/detail", [
       "title": "Test App",
       "heading": "Greeting Detail",
-      "greeting": greeting.makeNode()
+      "greeting": greeting.makeNode(),
+      "directives": directives
     ])
   }
 
   func prepare(_ request: Request) throws -> ResponseRepresentable {
     return try app.view.make("greeting/create", [
       "title": "Test App",
-      "heading": "Create A Greeting"
+      "heading": "Create A Greeting",
+      "directives": directives
     ])
   }
 
@@ -34,7 +54,8 @@ final class GreetingController {
     return try app.view.make("greeting/edit", [
       "title": "Test App",
       "heading": "Edit Greeting",
-      "greeting": greeting.makeNode()
+      "greeting": greeting.makeNode(),
+      "directives": directives
     ])
   }
 
@@ -60,17 +81,22 @@ final class GreetingController {
     return try app.view.make("greeting/table", [
       "title": "Test App",
       "heading": "All Greetings",
-      "greetings": Greeting.all().makeNode()
+      "greetings": Greeting.all().makeNode(),
+      "directives": directives
     ])
   }
 
   func list(_ request: Request) throws -> ResponseRepresentable {
+    var greetings: [Greeting] = []
     let list = request.data["greetings"]?.array.flatMap { $0.map { Int($0.string ?? "") ?? 0 } } ?? []
-    let greetings = try Greeting.query().filter("id", .in, list).all()
+    if !list.isEmpty {
+      greetings = try Greeting.query().filter("id", .in, list).all()
+    }
     return try app.view.make("greeting/table", [
       "title": "Test App",
       "heading": "Some Greetings",
-      "greetings": greetings.makeNode()
+      "greetings": greetings.makeNode(),
+      "directives": directives
     ])
   }
 
