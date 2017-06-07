879191c6a19d24faf725475c50b2f18aa6fcd7f1
Francois Green <fwgreen@users.noreply.github.com>
2017-06-06 23:54:43 -0400

    Update to Vapor 1.5
    PostgreSQL Provider is now host at vapor-community

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name: "PersonalGreetings",
   dependencies: [
-    .Package(url:"https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-    .Package(url:"https://github.com/vapor/postgresql-provider.git", majorVersion: 1, minor: 1)
+    .Package(url:"https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+    .Package(url:"https://github.com/vapor-community/postgresql-provider.git", majorVersion: 1, minor: 1)
   ],
   exclude: [
     "Config",
